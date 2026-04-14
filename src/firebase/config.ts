4dce23f266d0db4c047c86225db201c5f8f9d6fd
Pritam Debnath <pd863253@gmail.com>
2026-04-14 06:20:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5530229971-c739c",
+  "appId": "1:107592945143:web:1a323d5c58e9a26d92f2f2",
+  "apiKey": "AIzaSyAAoU1g7-uHlRnzOjhBbqaiPBE9Tb4fQSQ",
+  "authDomain": "studio-5530229971-c739c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "107592945143"
+};
